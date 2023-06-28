0e1616453c0a2855832d167bc391fb2c73fe8174
JungSooKim <gasalee123@gmail.com>
2023-06-28 12:20:01 +0900

    Model Complete

--- Model/BottomUp.py
+++ Model/BottomUp.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 
 class Encoder(nn.Module):
-    def __init__(self, name='resnet50'):
+    def __init__(self, backbone='resnet152'):
         super(Encoder, self).__init__()
 
-        model = torch.hub.load('pytorch/vision:v0.10.0', name, pretrained=True)
+        model = torch.hub.load('pytorch/vision:v0.10.0', backbone, pretrained=True)
         self._first_conv = self._make_first_conv(model)
 
         self.res_block_1 = model.layer1
@@ -32,4 +32,4 @@ class Encoder(nn.Module):
         c4 = self.res_block_3(c3)
         c5 = self.res_block_4(c4)
 
-        return [c2, c3, c4, c5]
+        return c2, c3, c4, c5
--- Model/TopDown.py
+++ Model/TopDown.py
@@ -6,14 +6,14 @@ class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
 
-        self.reduce_dim_1 = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)
-        self.reduce_dim_2 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
+        self.reduce_dim_5 = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)
+        self.reduce_dim_4 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
         self.reduce_dim_3 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0)
-        self.reduce_dim_4 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
+        self.reduce_dim_2 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
 
-        self.out_layer_1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.out_layer_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.out_layer_4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.out_layer_3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.out_layer_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
 
     def _up_add(self, x, y):
         (_, _, H, W) = y.size()
@@ -21,17 +21,18 @@ class Decoder(nn.Module):
 
         return x_up + y
 
-    def forward(self, x):
-        (c2, c3, c4, c5) = x
+    def forward(self, c2, c3, c4, c5):
+        # (c2, c3, c4, c5) = x
+        # print(x.size)
 
-        p5 = self.reduce_dim_4(c5)
-        m4 = self._up_add(p5, self.reduce_dim_3(c4))
+        p5 = self.reduce_dim_5(c5)
+        m4 = self._up_add(p5, self.reduce_dim_4(c4))
         m3 = self._up_add(m4, self.reduce_dim_3(c3))
-        m2 = self._up_add(m3, self.reduce_dim_3(c2))
+        m2 = self._up_add(m3, self.reduce_dim_2(c2))
 
         # output layer
-        p4 = self.out_layer_3(m4)
-        p3 = self.out_layer_2(m3)
-        p2 = self.out_layer_1(m2)
+        p4 = self.out_layer_4(m4)
+        p3 = self.out_layer_3(m3)
+        p2 = self.out_layer_2(m2)
 
-        return [p5, p4, p3, p2]
+        return p5, p4, p3, p2
